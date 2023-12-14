cfb67394843f468731e947cef77eeac2a4216820
abusithbot <aboubakar.03@hotmail.com>
2023-12-14 14:44:24 +0100

    animations
    marche

---
+++ Assets/Scripts/StepDetector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepDetector : MonoBehaviour
+{
+    private Vector3 FloorPosition;
+    private Vector3 StePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
