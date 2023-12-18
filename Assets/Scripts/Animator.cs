45bb0a21f3195f72f4dc8fc53ba9d6729200218e
abusithbot <aboubakar.03@hotmail.com>
2023-12-18 10:10:01 +0100

    modification Animator
    il est en idle mais il marche pas grace au scripte

--- Assets/Scripts/Animator.cs
+++ Assets/Scripts/Animator.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Animator : MonoBehaviour
 {
+    public Animator anim;
+    private PlayerController controller;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
+        controller = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      /*  anim.SetFloat("Forward/Backward", rb.velocity.magnitude);
+        anim.SetFloat("Left/", rb.velocity.magnitude);*/
     }
 }
